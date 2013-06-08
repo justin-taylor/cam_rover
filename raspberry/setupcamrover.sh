da335c94b6377b104aa19003d657407192598a28
Justin Taylor <justin@tayloredapps.org>
2013-06-08 12:15:45 -0500

    Turned on logging for the control server

--- raspberry/setupcamrover.sh
+++ raspberry/setupcamrover.sh
@@ -6,7 +6,7 @@ sudo service udhcpd start
 
 #start server
 cd /home/pi/cam_rover/raspberry/bin
-sudo ./main 9300 #2>&1 > /var/log/cam_server.log &
+sudo ./main 9300 2>&1 > /var/log/cam_server.log &
 
 #start camera
 cd /home/pi/webcam/
