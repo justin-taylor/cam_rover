e1acdefce5c47250d5cb22056b90e45284de509c
Justin Taylor <justin@tayloredapps.org>
2013-06-08 12:14:15 -0500

    finished setting up the wireless access point and running servers

--- raspberry/setupcamrover.sh
+++ raspberry/setupcamrover.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-# start up script to start all applications for the camrover
 
 #set static ip
 sudo ifconfig wlan0 192.168.42.1
 sudo service udhcpd start
 
 #start server
-#TODO
+cd /home/pi/cam_rover/raspberry/bin
+sudo ./main 9300 #2>&1 > /var/log/cam_server.log &
 
 #start camera
-#TODO
+cd /home/pi/webcam/
+./run_streamer.sh 2>&1 > /dev/null &
